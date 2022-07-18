8ed8ec45394043ba795f56018e3170db6ddeaf48
Keks <keks@htmlacademy.ru>
2022-07-18 11:07:49 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,6 @@
+import {drawMiniatures} from './drawMiniature.js';
+import {getRandomIntNumber, isStringRightLength} from './util.js';
+
+getRandomIntNumber(0, 2);
+isStringRightLength('123', 2);
+drawMiniatures();
---
+++ 7/js/util.js
@@ -0,0 +1,42 @@
+const getRandomIntNumber = function(minNumber, maxNumber) {
+  if(minNumber < 0 || maxNumber < 0){
+    return 'Диапазон должен быть больше 0';
+  }
+
+  if(maxNumber - minNumber < 1){
+    return 'Целое число в введенном диапазоне не найдено.';
+  }
+  const lower = Math.ceil(Math.min(Math.abs(minNumber), Math.abs(maxNumber)));
+  const upper = Math.floor(Math.max(Math.abs(minNumber), Math.abs(maxNumber)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+function createRandomIdFromRangeGenerator (min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomIntNumber(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomIntNumber(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+const isStringRightLength = function(checkedString, maxLength)
+{
+  return checkedString.length <= maxLength;
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomIntNumber(0, elements.length - 1)];
+
+const isEscapeKey = (evt) => evt.key === 'Escape';
+
+const isEnterKey = (evt) => evt.key === 'Enter';
+
+export {getRandomIntNumber, createRandomIdFromRangeGenerator, isStringRightLength, getRandomArrayElement, isEscapeKey, isEnterKey};
