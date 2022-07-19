fbf1ae956e1f7c3236d04d8b057e50a16e561101
Keks <keks@htmlacademy.ru>
2022-07-19 14:34:37 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/main.js
@@ -0,0 +1,8 @@
+import {drawMiniatures} from './drawMiniature.js';
+import {getRandomIntNumber, isStringRightLength} from './util.js';
+import {createUploadFileForm} from './formValidation-modal.js';
+
+getRandomIntNumber(0, 2);
+isStringRightLength('123', 2);
+drawMiniatures();
+createUploadFileForm();
